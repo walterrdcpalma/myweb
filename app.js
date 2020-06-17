27463b05b132096382b2788c1fd1a12faadb46e1
Walter Palma <walterrdcpalma@gmail.com>
2020-06-17 02:45:50 +0100

    Mudar o conteudo pelos botoes

---
+++ app.js
@@ -0,0 +1,41 @@
+function sobre() {
+  $("#container-conteudo-secundario2").css({
+    display: "none",
+  });
+
+  $("#container-conteudo-secundario").css({
+    display: "flex",
+  });
+
+  $("#container-conteudo-secundario3").css({
+    display: "none",
+  });
+}
+
+function projetos() {
+  $("#container-conteudo-secundario2").css({
+    display: "flex",
+  });
+
+  $("#container-conteudo-secundario").css({
+    display: "none",
+  });
+
+  $("#container-conteudo-secundario3").css({
+    display: "none",
+  });
+}
+
+function contactos() {
+  $("#container-conteudo-secundario2").css({
+    display: "none",
+  });
+
+  $("#container-conteudo-secundario").css({
+    display: "none",
+  });
+
+  $("#container-conteudo-secundario3").css({
+    display: "flex",
+  });
+}
